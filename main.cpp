f87bdcc5a1419c64824e7b3ac0bfa376e8dc4966
Thomas Lihoreau <thomas.lihoreau@epitech.eu>
2020-11-17 08:30:04 +0100

    [ADD] travis test function

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** Travis test main
+** File description:
+** .
+*/
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    std::cout << "Hey ! this is Travis !" << std::endl;
+    return (0);
+}
